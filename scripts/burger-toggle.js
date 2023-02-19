8228250300cb119ca2169008a7587b32d5f5d0dd
Sam McEwan <samlmcewan@nhs.scot>
2023-02-19 16:33:40 +0000

    Create header and nav

---
+++ scripts/burger-toggle.js
@@ -0,0 +1,10 @@
+const animatedIcon = document.querySelector(".animated-icon");
+const navbarToggler = document.querySelector("header .navbar-toggler");
+
+navbarToggler.addEventListener('click', (e) => {
+    
+        animatedIcon.classList.toggle("open");
+        
+    });
+                        
+              
